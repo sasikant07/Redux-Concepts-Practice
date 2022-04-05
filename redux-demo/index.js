d6dc2a24535c0d43ce9404008af41d8f67f0fa61
Sasikant Biswal <biswalsashikant@gmail.com>
2022-04-05 17:46:42 +0530

    Using multiple reducers in our simple javascript application

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,8 +1,10 @@
 const redux = require('redux');
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
 
 // Actions
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 // Action creator
 function buyCake() {
@@ -12,16 +14,41 @@ function buyCake() {
     }
 }
 
+function buyIceCream() {
+    return {
+        type: BUY_ICECREAM
+    }
+}
+
 // state of our application
 
-const initialState = {
+// const initialState = {
+//     numOfCakes: 10
+// }
+
+const initialCakeState = {
     numOfCakes: 10
+}
+
+const initialIcecreamState = {
+    numOfIceCream: 20
 }
 
 // reducer function
 // Reducer = (state, action) => returns newSatate
 
-const reducer = (state = initialState, action) => {
+// const reducer = (state = initialState, action) => {
+//     switch(action.type) {
+//         case BUY_CAKE: return {
+//             ...state,
+//             numOfCakes: state.numOfCakes - 1
+//         }
+
+//         default: return state;
+//     }
+// }
+
+const cakeReducer = (state = initialCakeState, action) => {
     switch(action.type) {
         case BUY_CAKE: return {
             ...state,
@@ -32,11 +59,28 @@ const reducer = (state = initialState, action) => {
     }
 }
 
+const iceCreamReducer = (state = initialIcecreamState, action) => {
+    switch(action.type) {
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCream: state.numOfIceCream - 1
+        }
+
+        default: return state;
+    }
+}
+
+// Combining multiple reducers
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+})
 
 // Store
 
 // create a store
-const store = createStore(reducer)
+// const store = createStore(reducer)
+const store = createStore(rootReducer)
 
 // store now exposes the getState() method()
 console.log('Initial state : ', store.getState());
@@ -48,6 +92,8 @@ const unSubscribe = store.subscribe(() => console.log('Updated state : ', store.
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 
 // unsubscribe the store
 unSubscribe()
