76b9485692a46a5f7cb98322ad72cb5f1b5eccf5
Sasikant Biswal <biswalsashikant@gmail.com>
2022-04-06 11:47:05 +0530

    Added  concepts using useSelector and useDispatch hooks in a new component

--- redux-basic-concepts/src/App.js
+++ redux-basic-concepts/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
         <CakeContainer />
+        <HooksCakeContainer />
       </Provider>
     </div>
   );
---
+++ redux-basic-concepts/src/components/HooksCakeContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { buyCake } from '../redux';
+
+function HooksCakeContainer() {
+    const numberOfCakes = useSelector(state => state.numOfCakes);
+    const dispatch = useDispatch();
+  return (
+    <div>
+        <h4>============================== Hooks Cake Conatiner ================================</h4>
+        <h2>Num of Cakes - {numberOfCakes}</h2>
+        <button onClick={() => dispatch(buyCake())}>Buy Cake</button>
+    </div>
+  )
+}
+
+export default HooksCakeContainer
