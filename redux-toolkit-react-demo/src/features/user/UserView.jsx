f532ecd3ac4a4623bc64de7112284d5f42e6e792
Sasikant Biswal <Sasikant.Biswal@lntinfotech.com>
2022-10-01 20:02:54 +0530

    Added code for redux toolkit with react components

---
+++ redux-toolkit-react-demo/src/features/user/UserView.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchUsers } from './userSlice';
+
+const UserView = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.user);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
+  return (
+    <div>
+      <h2>List of users</h2>
+      {user.loading && <div>Loading...</div>}
+      {!user.loading && user.error ? <div>Error: {user.error}</div> : null}
+      {!user.loading && user.users.length ? (
+        <ul>
+          {
+            user.users.map(user => {
+              return <li key={user.id}>{user.name}</li>
+            })
+          }
+        </ul>
+      ) : null}
+    </div>
+  )
+}
+
+export default UserView;
